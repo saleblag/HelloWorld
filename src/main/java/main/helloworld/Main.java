4001b27543f52462e5a7b124362d7d135f11a18e
Sasa Blagojevic <saleblag@gmail.com>
2020-02-04 20:22:32 +0100

    Adds age checker

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -1,5 +1,7 @@
 package main.helloworld;
 
+import java.util.Scanner;
+
 /**
  *
  * @author qa
@@ -13,6 +15,40 @@ public class Main {
         
         System.out.println("Hello World!");
         System.out.println("Sasa Blagojevic");
+        
+        int age = 5;
+        
+        if (age > 18) {
+            System.out.println("Osoba je punoletna");
+        } else {
+            System.out.println("Osoba je maloletna");
+        }
+        
+        System.out.print("Unesite broj godina: ");
+        Scanner input = new Scanner(System.in);
+        int userAge = input.nextInt();
+        
+        System.out.println("Korisnik je uneo: " + userAge);
+        
+        if (userAge > 18) {
+            System.out.println("Osoba je punoletna");
+        } else {
+            System.out.println("Osoba je maloletna");
+        }
+        
+        System.out.print("Unesite broj godina: ");
+               Scanner input2 = new Scanner(System.in);
+                int personAGe =input.nextInt();
+                
+        if (personAge <= 0);
+                System.out.println("Invalid");
+        if (personAge <= 5) {
+         System.out.println("Baby");
+         } else if (personAGe <= 11); {
+                 System.out.println("Kid");
+                 }
+                 
+                 }
     }
     
-}
+
