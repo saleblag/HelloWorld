4b26969b05c13a44b9688d27f1fc9886edfef949
Sasa Blagojevic <saleblag@gmail.com>
2020-02-04 18:48:58 +0100

    Initial commit

---
+++ src/main/java/main/helloworld/Main.java
@@ -0,0 +1,18 @@
+package main.helloworld;
+
+/**
+ *
+ * @author qa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        System.out.println("Sasa Blagojevic");
+    }
+    
+}
